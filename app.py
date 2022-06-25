8744115b96ea9ac885f61285d27e864b552f5442
Anand <raschoudhary879@gmail.com>
2022-06-25 15:02:40 +0530

    first commit

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask,render_template,request
 app=Flask(__name__)
 
+app = Flask(
+ __name__,
+ template_folder="./templates",
+ static_folder="./static",
+)
+
+@app.route("/json")
+def json_response():
+    response = {"name": "Pratap", "age": 24}
+    #return jasonify
+    return response
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -10,4 +22,4 @@ def pagenotfound(e):
     return render_template("error404.html")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True,port=5000,host='0.0.0.0')
