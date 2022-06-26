700712383dc5ab572ba41fb33805fef5f0a77ea9
Anand <raschoudhary879@gmail.com>
2022-06-26 16:31:31 +0530

    first commit

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 from flask import Flask,render_template,request
-app=Flask(__name__)
-
-app = Flask(
- __name__,
- template_folder="./templates",
- static_folder="./static",
+app=Flask(
+    __name__,
+    static_folder='static',
+    template_folder='templates'
 )
-
-@app.route("/json")
-def json_response():
-    response = {"name": "Pratap", "age": 24}
-    #return jasonify
-    return response
 
 @app.route("/")
 def home():
@@ -22,4 +14,4 @@ def pagenotfound(e):
     return render_template("error404.html")
 
 if __name__=="__main__":
-    app.run(debug=True,port=5000,host='0.0.0.0')
+    app.run(debug=True)
---
+++ static/script/home.js
@@ -0,0 +1,49 @@
+window.onload = function () {
+    if (window.localStorage.getItem("theme") == "dark") {
+        changeTheme(0);
+    } else if (window.localStorage.getItem("theme") == "light") {
+        changeTheme(1);
+    } else {
+        window.localStorage.setItem("theme", "dark");
+    }
+};
+window.onscroll = function () {
+    checkscroll();
+};
+$(document).ready(function () {
+    $("a").on("click", function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $("html, body").animate({
+                    scrollTop: $(hash).offset().top - 72,
+                },
+                800,
+                function () {
+                    window.location.hash = hash - 72;
+                }
+            );
+        }
+    });
+});
+
+
+
+// All Application's functions starts here
+const checkscroll=()=>{
+    if(window.scrollY>200){
+        document.getElementById("site-header").style.backgroundColor = "#000000";
+    }else{
+        document.getElementById("site-header").style.backgroundColor = "transparent";
+    }
+    if (window.scrollY > 100) {
+        document.getElementById("moveUp").style.animationDuration = "0.5s";
+        document.getElementById("moveUp").style.display = "block";
+        document.getElementById("themeBody").style.animationName = "slideUp";
+        document.getElementById("moveUp").style.animationName = "slideIn";
+    } else {
+        document.getElementById("moveUp").style.animationDuration = "0.25s";
+        document.getElementById("themeBody").style.animationName = "slideDown";
+        document.getElementById("moveUp").style.animationName = "slideOut";
+    }
+}
